c415c1b43ec235ed2805def0377e5f4ed9261958
Lenart GOlob <lenart.golob17@gmail.com>
2021-11-30 19:16:53 +0100

    cleaned imports

--- src/main/java/com/kumuluzee/xcontext/ActivityAdvisorResource.java
+++ src/main/java/com/kumuluzee/xcontext/ActivityAdvisorResource.java
@@ -1,21 +1,12 @@
 package com.kumuluzee.xcontext;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
 
 @RequestScoped
 @Consumes(MediaType.APPLICATION_JSON)
